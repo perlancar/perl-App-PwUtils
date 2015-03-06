84d639d0c52b329c00abdee93cacbc846438adf7
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-06 13:43:50 +0700

    Initial commit

---
+++ lib/App/UpfUtils.pm
@@ -0,0 +1,29 @@
+package App::UpfUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: CLI interface for Unix::Passwd::File (as separate scripts)
+
+=head1 SYNOPSIS
+
+See the included C<upf-*> scripts.
+
+
+=head1 DESCRIPTION
+
+This distribution includes CLI scripts C<upf-*>, one for each corresponding
+function in L<Unix::Passwd::File>.
+
+
+=head1 SEE ALSO
+
+L<Unix::Passwd::File>
+
+L<App::upf>, which includes a single CLI script L<upf> containing subcommands,
+instead of one script for each function like included in this distribution.
+
+=cut
